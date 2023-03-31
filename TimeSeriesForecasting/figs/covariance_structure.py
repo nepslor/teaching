90b9da9bba68f716715ef6cbf644686129ee2d54
nepslor <nepslor@gmail.com>
2023-03-31 12:04:34 +0200

    addedd covariance_structure.py and copula plots

---
+++ TimeSeriesForecasting/figs/covariance_structure.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal, norm
+import seaborn as sb
+from scipy.interpolate import interp1d
+from sklearn.covariance import GraphicalLasso, GraphicalLassoCV, ShrunkCovariance
+from TimeSeriesForecasting.utils.plot_utils import SeabornFig2Grid
+
+estimation = 'shrunk'
+N = 5000
+n_scen = 5000
+
+
+def get_pdf(x, q_vect=np.linspace(0, 1, 100)):
+    return np.quantile(x, q_vect, axis=0)
+
+
+def joinplot(x, q_vect=np.linspace(0, 1, 100)):
+    x1, x2 = x.T
+    pdfs = get_pdf(x, q_vect)
+    g1 = sb.jointplot(x=x1.ravel(), y=x2.ravel(), marker=".", s=10,
+                 marginal_kws=dict(bins=25, fill=False))
+
+    g1.ax_marg_x.plot(pdfs[:, 0], q_vect*N/20)
+    g1.ax_marg_y.plot(q_vect*N/20, pdfs[:, 1])
+    return g1
+
+
+def multivariate_normal_copula(x):
+    # estimate normalized covariance of the original signals
+    est_cov = np.corrcoef(x.T)
+
+    # sample from the multivariate normal distribution with the same covariance
+    mvr = multivariate_normal([0, 0], est_cov)
+    mvr_samples = mvr.rvs((n_scen))
+
+    # retrieve the copula samples (multivariate normal CDF)
+    return norm.cdf(mvr_samples)
+
+
+def sample_scenarios(n_scen, pdfs, c, q_vect=np.linspace(0, 1, 100)):
+    scenarios = np.zeros((n_scen, 2))
+    for i, pdf in enumerate(pdfs.T):
+        q_interpfun_at_step = interp1d(q_vect, pdf, fill_value='extrapolate')
+        scenarios[:, i] = q_interpfun_at_step(c[:, i])
+    return scenarios
+
+
+# generate correlated signals
+z = np.random.randn(int(N), 1) + 1.6
+x1 = z
+x2 = np.log(z+10) + 0.1*np.random.randn(N, 1)
+x = np.hstack([x1, x2])
+
+c = multivariate_normal_copula(x)
+pdfs = get_pdf(x)
+scens = sample_scenarios(n_scen, pdfs, c)
+
+g1 = joinplot(x)
+g2 = joinplot(c)
+g3 = joinplot(scens)
+SeabornFig2Grid([g1, g2, g3], figsize=(15, 5))
+plt.savefig('TimeSeriesForecasting/figs/covariance_structure_unimodal.png', dpi=300)
+
+z = np.vstack([np.random.randn(int(N/2), 1) + 1.6,  np.random.randn(int(N/2), 1)*0.8 + 6])
+x1 = z
+x2 = np.log(z+10)+ 0.1*np.random.randn(N, 1)
+x = np.hstack([x1, x2])
+
+c = multivariate_normal_copula(x)
+pdfs = get_pdf(x)
+scens = sample_scenarios(n_scen, pdfs, c)
+
+g1 = joinplot(x)
+g2 = joinplot(c)
+g3 = joinplot(scens)
+SeabornFig2Grid([g1, g2, g3], figsize=(15, 5))
+plt.savefig('TimeSeriesForecasting/figs/covariance_structure_bimodal.png', dpi=300)
--- TimeSeriesForecasting/utils/plot_utils.py
+++ TimeSeriesForecasting/utils/plot_utils.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+import seaborn as sns
+import matplotlib.gridspec as gridspec
+
+
 def ts_animation(y_te, y_hat, n_rows=50, y_min=-2, y_max=2):
     "plot the first n_rows of the two y_te and y_hat matrices"
     fig, ax = plt.subplots(1);
@@ -26,3 +30,70 @@ def ts_animation(y_te, y_hat, n_rows=50, y_min=-2, y_max=2):
     plt.close('all')
     # rc('animation', html='jshtml')
     return HTML(ani.to_jshtml())
+
+
+
+class SeabornFig2Grid():
+    """
+    Plot multiple distplots in different subplots.
+    Move a seaborn FacetGrid or PairGrid to a GridSpec within a figure.
+    """
+
+    def __init__(self, seaborngrid, figsize=(13, 8)):
+        fig = plt.figure(figsize=figsize)
+        gs = gridspec.GridSpec(1, len(seaborngrid))
+
+        for sbg, s in zip(seaborngrid, gs):
+            self.fig = fig
+            self.sg = sbg
+            self.subplot = s
+            if isinstance(self.sg, sns.axisgrid.FacetGrid) or \
+                isinstance(self.sg, sns.axisgrid.PairGrid):
+                self._movegrid()
+            elif isinstance(self.sg, sns.axisgrid.JointGrid):
+                self._movejointgrid()
+            self._finalize()
+            plt.show()
+        gs.tight_layout(fig)
+
+
+
+    def _movegrid(self):
+        """ Move PairGrid or Facetgrid """
+        self._resize()
+        n = self.sg.axes.shape[0]
+        m = self.sg.axes.shape[1]
+        self.subgrid = gridspec.GridSpecFromSubplotSpec(n,m, subplot_spec=self.subplot)
+        for i in range(n):
+            for j in range(m):
+                self._moveaxes(self.sg.axes[i,j], self.subgrid[i,j])
+
+    def _movejointgrid(self):
+        """ Move Jointgrid """
+        h= self.sg.ax_joint.get_position().height
+        h2= self.sg.ax_marg_x.get_position().height
+        r = int(np.round(h/h2))
+        self._resize()
+        self.subgrid = gridspec.GridSpecFromSubplotSpec(r+1,r+1, subplot_spec=self.subplot)
+
+        self._moveaxes(self.sg.ax_joint, self.subgrid[1:, :-1])
+        self._moveaxes(self.sg.ax_marg_x, self.subgrid[0, :-1])
+        self._moveaxes(self.sg.ax_marg_y, self.subgrid[1:, -1])
+
+    def _moveaxes(self, ax, gs):
+        #https://stackoverflow.com/a/46906599/4124317
+        ax.remove()
+        ax.figure=self.fig
+        self.fig.axes.append(ax)
+        self.fig.add_axes(ax)
+        ax._subplotspec = gs
+        ax.set_position(gs.get_position(self.fig))
+        ax.set_subplotspec(gs)
+
+    def _finalize(self):
+        plt.close(self.sg.fig)
+        self.fig.canvas.mpl_connect("resize_event", self._resize)
+        self.fig.canvas.draw()
+
+    def _resize(self, evt=None):
+        self.sg.fig.set_size_inches(self.fig.get_size_inches())
